af44615bcd000a3ae64ca8694957b8f029af3945
BİLAL <bllsfk.3984@gmail.com>
2022-03-10 18:55:26 +0300

    hw001 ödevi yapıldı.

--- src/FirstHw.java
+++ src/FirstHw.java
@@ -1,5 +1,8 @@
 public class FirstHw {
     public static void main(String[] args) {
         //TODO: Lütfen ekrana Merhaba Dünya! yazan bir java programı yazınız.
+        String metin="Merhaba Dünya";
+        System.out.println(metin);
+
     }
 }
