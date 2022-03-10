5e41053eb35543c2475228706495212fdfc53e19
AlkanTuncer <alkantuncer53@hotmail.com>
2022-03-10 18:58:28 +0300

    Alkan Tuncer ilk hafta ödev teslim.

---
+++ src/OperatorsHW1.java
@@ -0,0 +1,13 @@
+public class OperatorsHW1 {
+    public static void main(String[] args) {
+
+        int num1=17;
+        int num2=36;
+        System.out.println("Num1= " +num1);
+        System.out.println("Num2= " +num2);
+        int sum=num1+num2;
+        System.out.println(num1+"+"+num2+"="+sum);
+
+
+    }
+}
---
+++ src/OperatorsHW2.java
@@ -0,0 +1,14 @@
+public class OperatorsHW2 {
+    public static void main(String[] args) {
+
+        double num1=3;
+        System.out.println("Sayı 1=" +num1);
+        double num2=7;
+        System.out.println("Sayı 2=" +num2);
+        double num3=11;
+        System.out.println("Sayı 3=" +num2);
+        double result=(num1+num2+num3)/3;
+        System.out.println("Ortalaması="+result);
+
+    }
+}
