698b07dfb36a040306254ef4aa90d25c6d6b263e
Zeynep Düzen <99576262+ZeynepDuzen@users.noreply.github.com>
2022-03-14 12:54:41 -0700

    HW1

--- src/FirstHw.java
+++ src/FirstHw.java
@@ -1,5 +1,11 @@
 public class FirstHw {
+
     public static void main(String[] args) {
         //TODO: Lütfen ekrana Merhaba Dünya! yazan bir java programı yazınız.
+        greeting();
     }
+    public static void greeting(){
+        System.out.println("Hello world");
+    }
+
 }
