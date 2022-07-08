1b2bc6006a569f5ba6da64eaa7a9a9afb24ab945
Mümin <mozdamar90@hotmail.com>
2022-07-08 10:37:25 +0300

    first homework

--- src/FirstHw.java
+++ src/FirstHw.java
@@ -1,6 +1,6 @@
 public class FirstHw {
     public static void main(String[] args) {
         //TODO: Lütfen ekrana Merhaba Dünya! yazan bir java programı yazınız.
-        System.out.println(" Merhaba mümın");
+        System.out.println("mümın özdamar");
     }
 }
